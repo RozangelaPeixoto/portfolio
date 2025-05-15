5e59ec6813a08e25d84c8dd273c3787143361531
RozangelaPeixoto <roxpmelo@gmail.com>
2025-05-15 15:00:56 -0300

    adicionado modal para certificados

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,45 @@
 const inputs = document.querySelectorAll('.contato__efeito');
 
 for (const input of inputs) {
-    input.addEventListener('focusout', function() {
-        for (const valor of inputs){
-            if (valor.value != ""){
+    input.addEventListener('focusout', function () {
+        for (const valor of inputs) {
+            if (valor.value != "") {
                 valor.classList.add("has-content");
-            }else{
+            } else {
                 valor.classList.remove("has-content")
             }
         }
     })
 }
 
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const closeModal = document.querySelector('.close');
+
+document.querySelectorAll('.btn-certificado').forEach(button => {
+    button.addEventListener('click', () => {
+        const imgSrc = button.getAttribute('data-img');
+        modalImg.src = imgSrc;
+        modal.classList.remove('hidden');
+    });
+});
+
+document.querySelectorAll('.figure-certificado').forEach(figure => {
+    figure.addEventListener('click', () => {
+        const imgSrc = figure.getAttribute('data-img');
+        modalImg.src = imgSrc;
+        modal.classList.remove('hidden');
+    });
+});
+
+closeModal.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    modalImg.src = '';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.add('hidden');
+        modalImg.src = '';
+    }
+});
